9aea11e8676127317927ae3247481f814a84e672
psantueno <seba.antueno@gmail.com>
2022-09-16 02:17:47 -0300

    feat: add new routes

--- src/routes/operations.js
+++ src/routes/operations.js
@@ -1,2 +1,12 @@
 const express = require("express");
 const router = express.Router();
+
+// Controller //
+// const operationsController = require("../controllers/operationsController");
+
+// router.get('/', operationsController.list);
+// router.get('/', operationsController.detail);
+// router.get('/', operationsController.add);
+// router.get('/:id', operationsController.delete);
+
+module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,2 +1,12 @@
 const express = require("express");
 const router = express.Router();
+
+// Controller //
+// const usersController = require("../controllers/usersController");
+
+// router.get('/', usersController.list);
+// router.get('/', usersController.detail);
+// router.get('/', usersController.add);
+// router.get('/:id', usersController.delete);
+
+module.exports = router;
